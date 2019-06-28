1a6e8ab870d2af03bce81639830f5ec0e273324e
Issoufi Adam <issoufi.adam1@gmail.com>
2019-06-28 13:10:16 +0200

    adding a algorithm to detect the winner

--- engine.js
+++ engine.js
@@ -21,17 +21,17 @@ class Engine {
     ];
 
     this.nbRedToken = 21;
-    this.nbBlueToken = 21;
+    this.nbYellowToken = 21;
     this.lastPlayerWhoPlayed = undefined;
     this.winner = undefined;
 
     this.RED_PLAYER_ID = 'red';
-    this.BLUE_PLAYER_ID = 'blue';
+    this.YELLOW_PLAYER_ID = 'yellow';
     this.EMPTY_BOX = 0;
   }
 
   hasEnoughToken(currentPlayer) {
-    const playerPoint = (currentPlayer === this.RED_PLAYER_ID) ? this.nbRedToken : this.nbBlueToken;
+    const playerPoint = (currentPlayer === this.RED_PLAYER_ID) ? this.nbRedToken : this.nbYellowToken;
 
     return playerPoint > 0;
   }
@@ -45,7 +45,7 @@ class Engine {
       throw new Error(`The column number must be between 1 and 7!`);
     }
 
-    const playerExist = playerId === this.RED_PLAYER_ID || playerId === this.BLUE_PLAYER_ID;
+    const playerExist = playerId === this.RED_PLAYER_ID || playerId === this.YELLOW_PLAYER_ID;
     if (!playerExist) throw new Error(`The player ${playerId} is unknow !`);
 
     const isYourTurn = this.isYourTurn(playerId);
@@ -68,7 +68,7 @@ class Engine {
         if (playerId === this.RED_PLAYER_ID) {
           this.nbRedToken -= 1;
         } else {
-          this.nbBlueToken -= 1;
+          this.nbYellowToken -= 1;
         }
 
         hasPlayed = true;
@@ -79,6 +79,73 @@ class Engine {
     });
 
     this.board = newBoard;
+
+    const alignmentWasFound = this.searchAlignment(playerId, this.board);
+    if (alignmentWasFound) {
+      this.winner = playerId;
+    }
+  }
+
+  searchAlignment(currentPlayer, board) {
+    const horizontalAlignmentWasFound = this.checkHorizontalAlignments(currentPlayer, board);
+    const verticalAlignmentWasFound = this.checkVerticalAlignments(currentPlayer, board);
+
+    return horizontalAlignmentWasFound || verticalAlignmentWasFound;
+  }
+
+  checkHorizontalAlignments(currentPlayer, board) {
+    const allIndexesRowsOfBoard = [0, 1, 2, 3, 4, 5];
+    const allIndexesColumnsNeededToCheckAlignment = [0, 1, 2, 3];
+
+    const alignmentWasFound = allIndexesRowsOfBoard.some(startRow =>
+      allIndexesColumnsNeededToCheckAlignment.some(
+        startColumn => this.checkAlignment({
+          startColumn,
+          searchedPattern: currentPlayer,
+          array: board[startRow]
+        })
+      )
+    );
+
+    return alignmentWasFound;
+  }
+
+  checkVerticalAlignments(currentPlayer, board) {
+    const allIndexesColumnsOfBoard = [0, 1, 2, 3, 4, 5, 6];
+    const allIndexesColumnsNeededToCheckAlignment = [0, 1, 2];
+
+    const alignmentWasFound = allIndexesColumnsOfBoard.some(column => {
+      const rowsTransformedToColumns = board.map(row => row[column]);
+
+      return allIndexesColumnsNeededToCheckAlignment.some(
+        startColumn => this.checkAlignment({
+          startColumn,
+          searchedPattern: currentPlayer,
+          array: rowsTransformedToColumns
+        })
+      );
+    })
+
+    return alignmentWasFound;
+  }
+
+  checkAlignment({ startColumn, searchedPattern, array }) {
+    if (startColumn < 0 || startColumn > array.length) {
+      return false;
+    }
+
+    const endColumn = startColumn + 3;
+    const goodRange = endColumn <= array.length;
+    if (!goodRange) { return false; }
+
+    for (let currentColumn = startColumn; currentColumn <= endColumn; currentColumn += 1) {
+      const box = array[currentColumn];
+      const isPresent = box === searchedPattern;
+
+      if (!isPresent) { return false; }
+    }
+
+    return true;
   }
 
   getBoard() {
@@ -86,13 +153,13 @@ class Engine {
   }
 
   getPlayers() {
-    return [this.RED_PLAYER_ID, this.BLUE_PLAYER_ID];
+    return [this.RED_PLAYER_ID, this.YELLOW_PLAYER_ID];
   }
 
   getTokens() {
     return {
       [this.RED_PLAYER_ID]: this.nbRedToken,
-      [this.BLUE_PLAYER_ID]: this.nbBlueToken
+      [this.YELLOW_PLAYER_ID]: this.nbYellowToken
     };
   }
 
